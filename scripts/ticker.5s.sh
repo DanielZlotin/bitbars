2b7d1e6f798c896b4db9533749732da49217b829
DanielZlotin <zlotindaniel@gmail.com>
2021-01-12 15:11:00 +0200

    show bnb price

--- scripts/ticker.5s.sh
+++ scripts/ticker.5s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Prints BTC price, eth price and FAST gas
+# Prints BTC price, eth price and FAST gas, BNB price
 
 API_TOKEN="***REMOVED***"
 
@@ -34,7 +34,19 @@ ethPerBtc = ethBalance / float(btcBalance)
 print($ethPrice * ethPerBtc)
 """)
 
+bnbUsdPairAddress="0x1b96b92314c44b159149f7e0303511fb2fc4774f"
+busdAddress="0xe9e7cea3dedca5984780bafc599bd69add087d56"
+wbnbAddress="0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
+wbnbPairBalance=$(curl -s "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${wbnbAddress}&address=${bnbUsdPairAddress}&tag=latest" | parseJson "['result']")
+busdPairBalance=$(curl -s "https://api.bscscan.com/api?module=account&action=tokenbalance&contractaddress=${busdAddress}&address=${bnbUsdPairAddress}&tag=latest" | parseJson "['result']")
+bnbPrice=$(python3 -c """
+bnbBalance = $wbnbPairBalance / float(10**18)
+busdBalance = $busdPairBalance / float(10**18)
+print(busdBalance / float(bnbBalance))
+""")
+
 ethRoundPrice=$(fmt "$ethPrice")
 btcRoundPrice=$(fmt "$btcPrice")
+bnbRoundPrice=$(fmt "$bnbPrice")
 
-echo "💰${btcRoundPrice}💰${ethRoundPrice}⛽️${gasPrice}"
+echo "💰${btcRoundPrice}💰${ethRoundPrice}⛽️${gasPrice}💰${bnbRoundPrice}"
