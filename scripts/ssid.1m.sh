21c1705196201075997ac7c4cf2eb530d8a082bb
DanielZlotin <danielzlotin@users.noreply.github.com>
2023-09-14 10:21:12 +0300

    fix

--- scripts/ssid.1m.sh
+++ scripts/ssid.1m.sh
@@ -4,4 +4,4 @@
 
 SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F: '/ SSID/{print $2}')
 
-[ ! -z "${SSID// }" ] && echo "⏣$SSID"
+[ ! -z "${SSID// }" ] && echo "$SSID"
